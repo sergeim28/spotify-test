eb9d427f24b0d6b124a42cc593687d8c98a28c48
serhii <sergiymisyura28@gmail.com>
2021-06-23 06:27:26 -0500

    remove confidential keys

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export default {
   api: {
     baseUrl: 'https://api.spotify.com/v1/browse/',
     authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: 'b71ee7b0ee644c99b231efae349cebca',
-    clientSecret: 'c3b2bd042be4499488b0c0af0a5d9dcd',
+    clientId: '',
+    clientSecret: '',
   }
 }
